373270d37a5910b6d9cb3bd91e593763dc6ed2a2
Marcus Lampley <Marcus@Kathleens-Air.hsd1.ga.comcast.net>
2020-09-18 12:07:36 -0400

    ref user in maker replacing string

--- models/product.js
+++ models/product.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
-  maker: { type: String, required: true },
+  maker: { type: Schema.Types.ObjectId, ref: 'User'},
   price: { type: String, default: "0", required: true },
   category: { type: String, required: true },
   countInStock: { type: Number, default: 0, required: true },
