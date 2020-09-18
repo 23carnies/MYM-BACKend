2a0521abf0d3133c7cc16fda5a108d39027025ff
Marcus Lampley <Marcus@Kathleens-Air.hsd1.ga.comcast.net>
2020-09-18 10:21:57 -0400

    review schema added to user remove from product

--- models/product.js
+++ models/product.js
@@ -1,16 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const reviewSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    rating: { type: Number, default: 0 },
-    comment: { type: String, required: false },
-  },
-  {
-    timestamps: true,
-  }
-);
+
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
@@ -21,7 +12,6 @@ const productSchema = new mongoose.Schema({
   description: { type: String, required: true },
   rating: { type: Number, default: 0, required: true },
   numReviews: { type: Number, default: 0, required: true },
-  reviews: [reviewSchema],
 });
 
 
