96b5274a4505dad553783563850859c76e8cd529
Marcus Lampley <Marcus@Kathleens-Air.hsd1.ga.comcast.net>
2020-09-18 10:37:23 -0400

    made routes not as safe

--- routes/products.js
+++ routes/products.js
@@ -3,13 +3,16 @@ const productsCtrl = require('../controllers/products');
 
 // Public Routes
 router.get('/', productsCtrl.index);
-
+router.post('/', productsCtrl.create)
+router.get('/:id', productsCtrl.show)
+router.put('/:id', productsCtrl.update);
+router.delete('/:id', productsCtrl.delete);
 // Protected Routes
 router.use(require('../config/auth'));
-router.post('/', checkAuth, productsCtrl.create)
-router.get('/:id', checkAuth, productsCtrl.show)
-router.put('/:id', checkAuth, productsCtrl.update);
-router.delete('/:id', checkAuth, productsCtrl.delete);
+// router.post('/', checkAuth, productsCtrl.create)
+// router.get('/:id', checkAuth, productsCtrl.show)
+// router.put('/:id', checkAuth, productsCtrl.update);
+// router.delete('/:id', checkAuth, productsCtrl.delete);
 
 
 function checkAuth(req, res, next) {
