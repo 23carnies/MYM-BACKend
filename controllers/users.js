488b0c45f72d82de379a5f5b20000d55bed1364e
23carnies <46233990+23carnies@users.noreply.github.com>
2020-09-19 17:01:24 -0400

    Testing update user function

--- controllers/users.js
+++ controllers/users.js
@@ -2,8 +2,26 @@ const User = require("../models/user");
 
 module.exports = {
   index,
+  updateStoreToUser
 };
 
 function index(req, res) {
   User.find({}).then((users) => res.json(users));
 }
+
+async function updateStoreToUser(req, res) {
+  const userWithStore = await User.findByIdAndUpdate(req.params.id, req.body, {new:true}).then(() => {
+    console.log('backend', user)
+    res.status(200).json(userWithStore)
+  })
+}
+
+// async function updateStoreToUser(req, res) {
+//   const userWithStore = await User.findById(req.user._id)
+//   .then((user) => {
+//     user.store.push(Store._id)
+//     user.save().then(() => {
+//       res.status(200).json(userWithStore)
+//     })
+//   })
+// } 
