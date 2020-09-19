4389d16826b8003fd9d8311d305f1e1365aba003
23carnies <46233990+23carnies@users.noreply.github.com>
2020-09-19 16:41:49 -0400

    Test

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,27 @@
 const User = require("../models/user");
+const Store = require("../models/store")
 
 module.exports = {
   index,
+  //updateStoreToUser,
 };
 
 function index(req, res) {
   User.find({}).then((users) => res.json(users));
 }
+
+// async function updateStoreToUser(req, res) {
+//   const userWithStore = await User.findByIdAndUpdate(req.params.id, req.body, {new:true})
+//   console.log(user)
+//   res.status(200).json(userWithStore)
+// }
+
+// async function updateStoreToUser(req, res) {
+//   const userWithStore = await User.findById(req.user._id)
+//   .then((user) => {
+//     user.store.push(Store._id)
+//     user.save().then(() => {
+//       res.status(200).json(userWithStore)
+//     })
+//   })
+// }
