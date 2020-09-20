fbfd7904472a9af5aafd7d949f5da773b8c7d014
23carnies <46233990+23carnies@users.noreply.github.com>
2020-09-19 21:28:44 -0400

    more tests

--- controllers/stores.js
+++ controllers/stores.js
@@ -19,8 +19,9 @@ module.exports = {
   // }
   async function create(req, res) {
     const store = await Store.create(req.body)
-    .then(store => {res.json(store)})
+    .then(store => {res.json(store)
     .then(() => User.findByIdAndUpdate(req.user.id, user.store.push(store.id), {new: true}))
+  })
     console.log('user', req.user.id, 'store', store.id)
     .catch(err => {res.json(err)})
   }
